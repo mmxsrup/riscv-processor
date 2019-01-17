222f1ec578f63463a0aa14d3ed9415c0e456660b
mmxsrup <mmxsrup@yahoo.co.jp>
2019-01-18 01:08:37 +0900

    simplified because writing process is not done by icache

--- modules/cache/icache.sv
+++ modules/cache/icache.sv
@@ -18,8 +18,8 @@ module icache #(
 	axi_lite_if.master m_axi
 );
 
-	typedef enum logic [3 : 0] {
-		IDLE, ALLOCATE, RADDR, RDATA, WADDR, WDATA, WRESP
+	typedef enum logic [1 : 0] {
+		IDLE, RADDR, RDATA, ALLOCATE
 	} state_type;
 	state_type state, next_state;
 
@@ -27,32 +27,33 @@ module icache #(
 	logic [31 : 0] rdata;
 	logic hit;
 
-	assign ready = (state == IDLE) ? 1 : 0;
-	assign ram_rdata = (state == RDATA) ? m_axi.rdata : 0;
+	assign ready = (hit) ? 1 : 0;
+	assign ram_rdata = (state == RDATA) ? m_axi.rdata : ram_rdata;
 
 	// AR
 	assign m_axi.araddr  = (state == RADDR) ? addr : 32'h0;
 	assign m_axi.arvalid = (state == RADDR) ? 1 : 0;
 
-	// Rgit log
+	// R
 	assign m_axi.rready = (state == RDATA) ? 1 : 0;
 
 	// AW
-	assign m_axi.awvalid = (state == WADDR) ? 1 : 0;
-	assign m_axi.awaddr  = (state == WADDR) ? addr : 32'h0;
+	assign m_axi.awvalid = 0;
+	assign m_axi.awaddr  = 32'h0;
 
 	// W
-	assign m_axi.wvalid = (state == WDATA) ? 1 : 0;
-	assign m_axi.wdata  = (state == WDATA) ? rdata : 32'h0;
+	assign m_axi.wvalid = 0;
+	assign m_axi.wdata  = 32'h0;
 	assign m_axi.wstrb  = 4'b0000;
 
 	// B
-	assign m_axi.bready = (state == WRESP) ? 1 : 0;
+	assign m_axi.bready = 0;
 
 
 	cachemem #(.INIT_FILE(INIT_FILE)) cachemem (
 		.clk(clk), .rst_n(rst_n),
-		.en(valid), .we(4'b0000), .allocate((state == ALLOCATE) ? 1'b1 : 1'b0),
+		.en(valid), .we((state == ALLOCATE) ? 4'b1111 : 4'b0000),
+		.allocate((state == ALLOCATE) ? 1'b1 : 1'b0),
 		.addr(addr), .wdata((state == ALLOCATE) ? ram_rdata : 0), .rdata(rdata),
 		.hit(hit)
 	);
@@ -60,13 +61,10 @@ module icache #(
 
 	always_comb begin
 		case (state)
-			IDLE : next_state = (!hit && valid) ? ALLOCATE : IDLE;
-			ALLOCATE : next_state = IDLE;
+			IDLE : next_state = (!hit && valid) ? RADDR : IDLE;
 			RADDR : if (m_axi.arvalid && m_axi.arready) next_state = RDATA;
 			RDATA : if (m_axi.rvalid  && m_axi.rready ) next_state = ALLOCATE;
-			WADDR : if (m_axi.awvalid && m_axi.awready) next_state = WDATA;
-			WDATA : if (m_axi.wvalid  && m_axi.wready ) next_state = WRESP;
-			WRESP : if (m_axi.bvalid  && m_axi.bready ) next_state = RADDR;
+			ALLOCATE : next_state = IDLE;
 			default : next_state = IDLE;
 		endcase
 	end
